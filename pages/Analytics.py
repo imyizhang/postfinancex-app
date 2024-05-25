7b5b4b937e115a8f981146898f34f160f3b8fa30
Dominica-of <d.amanfo@4th-ir.com>
2024-05-25 14:07:08 +0200

    Added New Page

---
+++ pages/Analytics.py
@@ -0,0 +1,11 @@
+import postfinance
+import streamlit as st
+
+# Title
+st.title("📊 Analytics")
+st.caption("👁️🐝Ⓜ️ Powered by Tableau")
+
+# Info
+st.info(
+    "This page provides a comprehensive report on the type of calls received"
+)
